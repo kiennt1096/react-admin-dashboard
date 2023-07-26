2e4b034e2e7f49a78b3647d3f2beaf40808d3aed
Nguyen Trung Kien <kiennt3@rikkeisoft.com>
2023-07-26 13:31:48 +0900

    35:17 theme with light and dark mode

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
-
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+// CssBaseline dùng để reset lại Css về mặc định
 function App() {
     return <div className="app"></div>;
 }
