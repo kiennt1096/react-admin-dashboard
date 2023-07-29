b17cfe1858663612837f8eb8d54e38c64ab7ffc5
Nguyen Trung Kien <kiennt3@rikkeisoft.com>
2023-07-30 00:18:16 +0900

    bar chart

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import SidebarX from "./scenes/global/Sidebar";
 import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
-// import Bar from "./scenes/bar";
+import Bar from "./scenes/bar";
 import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
@@ -36,6 +36,7 @@ function App() {
                             <Route path="/form" element={<Form />} />
                             <Route path="/calendar" element={<Calendar />} />
                             <Route path="/faq" element={<FAQ />} />
+                            <Route path="/bar" element={<Bar />} />
                             {/* <Route path="/pie" element={<Pie />} /> */}
                             {/* <Route path="/line" element={<Line />} /> */}
                             {/* <Route path="/geography" element={<Geography />} /> */}
