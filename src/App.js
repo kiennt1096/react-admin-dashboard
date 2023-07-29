d521ffe275b00fd4343f8247a706a1c1ae5ed07b
Nguyen Trung Kien <kiennt3@rikkeisoft.com>
2023-07-30 00:30:07 +0900

    piechart

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Contacts from "./scenes/contacts";
 import Bar from "./scenes/bar";
 import Form from "./scenes/form";
 // import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
+import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
 import Calendar from "./scenes/calendar";
@@ -37,7 +37,7 @@ function App() {
                             <Route path="/calendar" element={<Calendar />} />
                             <Route path="/faq" element={<FAQ />} />
                             <Route path="/bar" element={<Bar />} />
-                            {/* <Route path="/pie" element={<Pie />} /> */}
+                            <Route path="/pie" element={<Pie />} />
                             {/* <Route path="/line" element={<Line />} /> */}
                             {/* <Route path="/geography" element={<Geography />} /> */}
                             {/* <Route path="/calendar" element={<Calendar />} /> */}
